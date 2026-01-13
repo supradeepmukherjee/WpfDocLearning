2cb7f42c49181336354025ade25d88c1c2d5eac9
Supradeep Mukherjee <supradeep2004@gmail.com>
2026-01-13 19:04:41 +0530

    day4

---
+++ ExpenseAmountToBrushConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfDocLearning
+{
+    public class ExpenseAmountToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Brushes.Black;
+
+            if (double.TryParse(value.ToString(), out double amount))
+            {
+                return amount > 1000 ? Brushes.Red : Brushes.Black;
+            }
+
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
